5755723679c488713bce9a1a10cc5a5edfce7683
sonak <ghwn5833@gmail.com>
2023-05-27 14:36:28 +0900

    Todo create 기능 구현

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from todo.views import todos, index
+from todo.views import create_todo, todos, index
 
 urlpatterns = [
-    path('', todos),
+    path('', todos, name="todos"),
     path('index/', index),
+    path('create_todo/', create_todo, name="create"),
 ]
