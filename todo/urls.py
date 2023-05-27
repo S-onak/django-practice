af686d27a6068895960c1fddd8d9b34a7c2fdca0
sonak <ghwn5833@gmail.com>
2023-05-27 16:55:55 +0900

    과제

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from todo.views import create_todo, todos, index, update_todo, delete_todo
+from todo.views import complete_todo, create_todo, todos, index, update_todo, delete_todo
 
 urlpatterns = [
     path('', todos, name="todos"),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('create_todo/', create_todo, name="create"),
     path('update_todo/<int:pk>/', update_todo, name='update'),
     path('delete_todo/<int:pk>/', delete_todo, name='delete'),
+    path('complete_todo/<int:pk>/', complete_todo, name='complete'),
 ]
